4f4e7e15842a8ad32b32cb595c5f25b393df58d9
Arturo <art.leg.rod@gmail.com>
2017-04-02 16:44:29 +0200

    Test profiles.

---
+++ api/src/test/java/com/arturo/jm2api/common/ProfilesTest.java
@@ -0,0 +1,18 @@
+package com.arturo.jm2api.common;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+@ActiveProfiles(Profiles.DEVEVELOPMENT)
+public class ProfilesTest {
+
+    @Test
+    public void constructor() throws Exception {
+
+    }
+}
